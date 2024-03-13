a24cca9380c733c1e8ac78d8443ee8ec390f20b6
dev-david-alves <davidalves9090@gmail.com>
2024-03-13 09:49:27 -0300

    Blink and positions updated!

--- classes/input.js
+++ classes/input.js
@@ -3,22 +3,37 @@ class CustomInput {
     this.x = x;
     this.y = y;
     this.selected = true;
-    this.value = "";
-    this.parentColor = parentColor;
     this.color = { r: 0, g: 0, b: 0 };
+    this.parentColor = parentColor;
+    this.value = "";
+    this.modifiedText = "";
+    this.texMap = texMap;
     
     this.h = 20;
     this.w = textWidth(this.value) + 20;
     this.cursorY = this.y - (this.h * 0.8) / 2;
     this.cursorX = this.x;
 
-    this.texMap = texMap;
-    this.modifiedText = "";
+    // Blinking cursor
+    this.blinkInterval = 500;
+    this.lastBlinkTime = 0;
+    this.textToShow = "Blinking Text";
+    this.isVisible = true;
+    this.isTyping = false;
   }
   
   update() {
     this.w = textWidth(this.modifiedText) + 20;
+    this.cursorY = this.y - (this.h * 0.8) / 2;
     this.cursorX = this.x - this.w / 2 + textWidth(this.modifiedText) + 11;
+
+    if(this.isTyping) {
+      this.isVisible = true;
+      this.lastBlinkTime = millis();
+    } else if (millis() - this.lastBlinkTime >= this.blinkInterval) {
+      this.isVisible = !this.isVisible;
+      this.lastBlinkTime = millis();
+    }
 
     this.modifiedText = this.value;
     for(let key in this.texMap) {
@@ -36,10 +51,19 @@ class CustomInput {
   keyTyped() {
     if(!this.selected) return;
 
-    if(keyCode === BACKSPACE) {
+    if(keyCode === BACKSPACE || keyCode === DELETE || keyCode === ENTER) {
       return;
     } else {
       this.value += key;
+      this.isTyping = true;
+    }
+  }
+
+  keyReleased() {
+    if(!this.selected) return;
+
+    if(this.isTyping) {
+      this.isTyping = false;
     }
   }
 
@@ -48,21 +72,18 @@ class CustomInput {
 
     if(keyCode === BACKSPACE) {
       this.value = this.value.slice(0, -1);
+      this.isTyping = true;
     }
   }
 
   draw() {
     push();
-      fill(255);
-      stroke(this.color.r, this.color.g, this.color.b);
-      strokeWeight(1);
-      rect(this.x - this.w / 2, this.y - this.h / 2, this.w, this.h);
-      fill(this.color.r, this.color.g, this.color.b);
       noStroke();
+      fill(this.color.r, this.color.g, this.color.b);
       textAlign(CENTER, CENTER);
       text(this.modifiedText, this.x, this.y);
 
-      if(this.selected) {
+      if(this.selected && this.isVisible) {
         stroke(this.color.r, this.color.g, this.color.b);
         strokeWeight(1);
         line(this.cursorX, this.cursorY, this.cursorX, this.cursorY + this.h * 0.8);
